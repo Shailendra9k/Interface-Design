361bd555154cf02252ecd1c7372bae9f174d7d1b
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-04-18 23:05:16 +0300

    added changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "boostrap/dist/css/boostrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "./css/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "jquery/dist/jquery.js";
 import "popper.js/dist/umd/popper.js";
-// import "boostrap/dist/js/boostrap.js";
+import "bootstrap/dist/js/bootstrap.js";
 ReactDOM.render(
   <React.StrictMode>
     <App />
