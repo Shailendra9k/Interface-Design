6f8f081666c30b954759d89451ef3c90a6b16c01
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-04-19 09:03:36 +0300

    state defined

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/App.css";
 import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Shailendra",
+    };
+  }
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.name}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
