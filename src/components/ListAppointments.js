46e2f187bdad2eb846f2ebfe551c4cd657899f27
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-04-19 07:59:50 +0300

    listing component added and imported to parent

---
+++ src/components/ListAppointments.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class ListAppointments extends Component {
+  render() {
+    return <div>ListAppointments here</div>;
+  }
+}
+export default ListAppointments;
