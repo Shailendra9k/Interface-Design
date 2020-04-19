391994dcc59164cec0403a976ca539b7c82abe97
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-04-19 07:52:41 +0300

    addappointment added and tested

---
+++ src/components/AddAppointments.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class AddAppointments extends Component {
+  render() {
+    return <div>AddAppointments: make your appointments</div>;
+  }
+}
+export default AddAppointments;
