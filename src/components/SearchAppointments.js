f796fc9496a68dbbdadd7d388b85dc8505bc43e2
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-04-19 07:58:23 +0300

    search component added and imported to parent

---
+++ src/components/SearchAppointments.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class SearchAppointments extends Component {
+  render() {
+    return <div>SearchAppointments: Get your appointments</div>;
+  }
+}
+export default SearchAppointments;
